9ad2897da8497c9b73a8a13904a2b9cbb80e28c6
DiceR011 <touhu0305@gmail.com>
2024-10-16 14:17:19 +0900

    唐揚げ

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KarageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/karage', function(){
+    return view('karage');
 });
 
 Route::get('/dashboard', function () {
