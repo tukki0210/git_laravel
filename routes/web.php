08c975aaa2da1db0da7ca82fd14fc37e877c73b2
ID-hayato <hayto.romle@gmil.com>
2024-10-16 14:27:58 +0900

    肉を塩で焼けばなんでもうまい。

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/niku',function(){
+    return view('niku');
+});
 
 Route::get('/', function () {
     return view('welcome');
