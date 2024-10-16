05644abb100f5c5e53a746bc20816ceaac366c32
Aizawa <sj.116tlgb.pro418@gmail.com>
2024-10-16 14:30:08 +0900

    餃子01

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/gyoza01', function () {
+    return view('welcome');
+});
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
